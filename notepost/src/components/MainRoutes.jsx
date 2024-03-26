86f7a930f33be36e05703f3cd8224434413c6511
anyajegorova <anna.jerm@outlook.com>
2024-03-26 20:54:34 +0200

    routes modified, removed address links

--- notepost/src/components/MainRoutes.jsx
+++ notepost/src/components/MainRoutes.jsx
@@ -8,12 +8,12 @@ import Favourites from '../views/Favourites';
 const MainRoutes = ({ loggedIn, setLoggedIn }) => {
     return (
         <Routes>
-            <Route path='https://sayit-api.onrender.com/login' element={<Login mode='login' setLoggedIn={setLoggedIn} />} />
-            <Route path='https://sayit-api.onrender.com/register' element={<Login mode='register' setLoggedIn={setLoggedIn} />} />
-            {loggedIn ? (<Route path='https://sayit-api.onrender.com/favourites' element={<Favourites />} />) : null}
-            {loggedIn ? <Route path='https://sayit-api.onrender.com/profile' element={<Profile />} /> : null}
-            {loggedIn ? (<Route path='https://sayit-api.onrender.com/all_noteposts' element={<UserPosts mode='edit' />} />) : (<Route path='https://sayit-api.onrender.com/user_noteposts' element={<Login mode='login' setLoggedIn={setLoggedIn} />} />)}
-            <Route path='https://sayit-api.onrender.com/public_noteposts' element={<AllNoteposts mode='public' />} />
+            <Route path='/login' element={<Login mode='login' setLoggedIn={setLoggedIn} />} />
+            <Route path='/register' element={<Login mode='register' setLoggedIn={setLoggedIn} />} />
+            {loggedIn ? (<Route path='/favourites' element={<Favourites />} />) : null}
+            {loggedIn ? <Route path='/profile' element={<Profile />} /> : null}
+            {loggedIn ? (<Route path='/all_noteposts' element={<UserPosts mode='edit' />} />) : (<Route path='/user_noteposts' element={<Login mode='login' setLoggedIn={setLoggedIn} />} />)}
+            <Route path='/public_noteposts' element={<AllNoteposts mode='public' />} />
 
         </Routes>
     )
