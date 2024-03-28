eb4d898ace08c99bd1a307fec22db8f2465bf771
anyajegorova <anna.jerm@outlook.com>
2024-03-28 19:31:34 +0200

    edited: create post area style

--- notepost/src/components/AlertModal.jsx
+++ notepost/src/components/AlertModal.jsx
@@ -15,7 +15,28 @@ const AlertModal = ({ deleteNotepost, setShowAlert }) => {
             <div className='alert_container'>
                 <div className='modal_alert'>
                     <div className='content_container'>
-                        <div id='closeAlert' onClick={closeModal}>✖</div>
+                        <div id='closeAlert' onClick={closeModal}>
+                            <svg viewBox="0 0 24 24"
+                                fill="none"
+                                xmlns="http://www.w3.org/2000/svg">
+                                <g id="SVGRepo_bgCarrier" strokeWidth="0">
+                                </g>
+                                <g id="SVGRepo_tracerCarrier" strokeLinecap="round"
+                                    strokeLinejoin="round">
+                                </g>
+                                <g id="SVGRepo_iconCarrier">
+                                    <g id="Menu / Close_SM">
+                                        <path id="Vector"
+                                            d="M16 16L12 12M12 12L8 8M12 12L16 8M12 12L8 16"
+                                            stroke="#000000"
+                                            strokeWidth="2"
+                                            strokeLinecap="round"
+                                            strokeLinejoin="round">
+                                        </path>
+                                    </g>
+                                </g>
+                            </svg>
+                        </div>
                         <p>Are you sure you want to delete the post? The post will be deleted permanently.</p>
                         <div id='alert_buttons'>
                             <button onClick={closeModal} id='no'>No</button>
--- notepost/src/components/MobileMenu.jsx
+++ notepost/src/components/MobileMenu.jsx
@@ -13,7 +13,28 @@ const MobileMenu = ({ isMenuOpen, onMenuClick, logout, loggedIn }) => {
     return (
         <>
             <div className={isMenuOpen ? 'mobile_menu' : 'mobile_menu_hidden'}>
-                <h4 onClick={handleClick}>🗙</h4>
+                <h4 onClick={handleClick}>
+                    <svg viewBox="0 0 24 24"
+                        fill="none"
+                        xmlns="http://www.w3.org/2000/svg">
+                        <g id="SVGRepo_bgCarrier" strokeWidth="0">
+                        </g>
+                        <g id="SVGRepo_tracerCarrier" strokeLinecap="round"
+                            strokeLinejoin="round">
+                        </g>
+                        <g id="SVGRepo_iconCarrier">
+                            <g id="Menu / Close_SM">
+                                <path id="Vector"
+                                    d="M16 16L12 12M12 12L8 8M12 12L16 8M12 12L8 16"
+                                    stroke="#ffffff"
+                                    strokeWidth="2"
+                                    strokeLinecap="round"
+                                    strokeLinejoin="round">
+                                </path>
+                            </g>
+                        </g>
+                    </svg>
+                </h4>
                 <ul>
                     {loggedIn ? (
                         <>
